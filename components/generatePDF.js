fee370c09bf98ca2dd6c95e51605e59055588aa7
hyamy2025 <hichem_bensoltane@yahoo.com>
2025-06-21 15:54:08 +0100

    ✅V58

--- components/generatePDF.js
+++ components/generatePDF.js
@@ -72,9 +72,9 @@ export function generatePDF({ sallesSummary, apprenantsSummary, resultatsTable }
     const textWidth = pdf.getTextWidth(title);
     const rectX = (pageWidth - textWidth) / 2 - paddingH;
     const rectY = currentY - 10;
-    const rectWidth = textWidth + 2 * paddingH;
-    const rectHeight = 10 + 2 * paddingV;
-    pdf.roundedRect(rectX, rectY, rectWidth, rectHeight, 1, 1, 'S'); // 'S' = Stroke فقط (بدون تعبئة)
+    const rectWidth = textWidth + 1 * paddingH;
+    const rectHeight = 10 + 1 * paddingV;
+    pdf.roundedRect(rectX, rectY, rectWidth, rectHeight, 2, 2, 'S'); // 'S' = Stroke فقط (بدون تعبئة)
     pdf.setTextColor(0, 0, 0);         // لون العنوان أسود
     pdf.text(title, pageWidth / 2, currentY, { align: 'center' });
     pdf.setTextColor(0, 0, 0);
