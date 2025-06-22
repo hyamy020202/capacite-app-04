a4e3d613f10792dc5c64ba3e8a27e2095917c6fa
hyamy2025 <hichem_bensoltane@yahoo.com>
2025-06-22 15:18:23 +0100

    ✅V80

--- pages/index.js
+++ pages/index.js
@@ -37,20 +37,21 @@ export default function Home() {
           style={{ width: 360, height: 60, objectFit: 'contain' }}
         />
         <h1
-    className="
-      text-lg sm:text-xl md:text-2xl font-bold text-center text-gray-800 mb-3
-      inline-block
-      px-6 py-2
-      border-2 border-black
-      rounded-xl
-      bg-white
-      shadow-sm
-    "
-  >
+          className="
+    text-base sm:text-lg md:text-xl font-bold text-center text-gray-800 mb-3
+    inline-block
+    px-4 py-1
+    border border-black
+    rounded-xl
+    bg-white
+    shadow-sm
+    mx-auto
+  "
+        >
           Simulateur de Diagnostic de la Capacité d&apos;Accueil
         </h1>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-6 text-center">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-6 text-left">
           <div>
             <label className="block text-sm font-medium text-gray-700 mb-1">
               Nom de la structure
